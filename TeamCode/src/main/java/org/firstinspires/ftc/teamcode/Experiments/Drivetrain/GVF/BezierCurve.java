25bb488f31c38d8d2e2c4f4f739096607d9e8072
daniel.yang <daniel.yang@students.tka.org>
2024-10-03 09:31:54 -0700

    fixed path forward

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Experiments/Drivetrain/GVF/BezierCurve.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Experiments/Drivetrain/GVF/BezierCurve.java
@@ -11,6 +11,15 @@ public class BezierCurve {
     //Declare parametric parameter t
     double T;
 
+    //Declare current closest point on Bezier curve
+    double closest_T;
+    double epochs = 5;
+    double rate = 0.1;
+
+    // Weighs the tangent vector
+    // The distance at which the robot corrects at 45 degrees
+    double tangent_weight = 2;
+
     public BezierCurve(Point p0, Point p1, Point p2){
         // initialize parametric parameter as 0
         this.T = 0.0;
@@ -21,8 +30,22 @@ public class BezierCurve {
         this.p2 = p2;
     }
 
-    public double length(Point p) {
-        return Math.sqrt(p.x*p.x+p.y*p.y);
+    // Basic math operations for Points
+    public static Point add_v(Point a, Point b) {
+        return new Point(a.x+b.x, a.y+b.y);
+    }
+
+    public static Point sub_v(Point a, Point b) {
+        return new Point(a.x-b.x, a.y-b.y);
+    }
+
+    public static double length(Point v) {
+        return Math.sqrt(v.x*v.x+v.y*v.y);
+    }
+
+    public static Point scale_v(Point v, double len) {
+        double scale_factor = len/length(v);
+        return new Point(v.x*scale_factor, v.y*scale_factor);
     }
 
     public Point derivative(double t){
@@ -46,15 +69,35 @@ public class BezierCurve {
         return new Point(x, y);
     }
 
+    //generates evenly spaced t with specified spacing
     public ArrayList<Double> arc_length_param(double dist) {
         ArrayList<Double> even_t = new ArrayList<>();
         double last_t = 0.0;
         even_t.add(0.0);
         while (last_t < 1) {
-            double dDdt = this.length(this.derivative(last_t));
-            last_t += dist/dDdt;
+            last_t += dist/length(this.derivative(last_t));
             even_t.add(last_t);
         }
         return even_t;
+    }
+
+    public double dDdt(Point p, double t) {
+        Point orth_v = sub_v(forward(t), p);
+        return orth_v.dot(derivative(t))/length(orth_v);
+    }
+
+    // Update the closest point using gradient descent
+    public void update_closest(Point p) {
+        for (int i = 0; i < epochs; i++) {
+            closest_T -= rate*dDdt(p, closest_T);
+        }
+    }
+
+    // Gets the vector given a robot's position
+    public Point get_v(Point p, double speed) {
+        update_closest(p);
+        Point orth_v = sub_v(forward(closest_T), p);
+        Point tangent_v = scale_v(derivative(closest_T), tangent_weight);
+        return scale_v(add_v(orth_v, tangent_v), speed);
     }
 }
