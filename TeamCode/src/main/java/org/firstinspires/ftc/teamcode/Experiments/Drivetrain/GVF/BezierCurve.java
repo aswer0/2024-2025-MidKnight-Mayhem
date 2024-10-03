5443826b0999447adecac2298694e00002cb16dd
daniel.yang <daniel.yang@students.tka.org>
2024-10-02 17:34:48 -0700

    fixed path forward

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Experiments/Drivetrain/GVF/BezierCurve.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Experiments/Drivetrain/GVF/BezierCurve.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.Experiments.Drivetrain.GVF;
 import org.opencv.core.Point;
+import java.util.ArrayList;
 
 public class BezierCurve {
     //Declare Control Points p0, p1, p2 as x and y pair
@@ -18,31 +19,42 @@ public class BezierCurve {
         this.p0 = p0;
         this.p1 = p1;
         this.p2 = p2;
-
     }
 
-    public double derivative(double t){
+    public double length(Point p) {
+        return Math.sqrt(p.x*p.x+p.y*p.y);
+    }
+
+    public Point derivative(double t){
         //derivative of quadratic bezier curve with respect to x
         double dx = 2*(1-t)*(p1.x-p0.x) + 2*t*(p2.x-p1.x);
         //derivative of quadratic bezier curve with respect to y
         double dy = 2*(1-t)*(p1.y-p0.y) + 2*t*(p2.y-p1.y);
 
         //final derivative change in x over change in y
-        return dy/dx;
+        return new Point(dx, dy);
     }
 
-    public Point forward(double t){
+    public Point forward(double t) {
         //set new parametric parameter
         this.T = t;
 
         //calculate x and y of quadratic bezier curve as a parametric
-        double x = ((1-t)*(1-t))*p0.x + 2*(1-t)*t*p1.x + (t*t)*p2.x;
-        double y = ((1-t)*(1-t))*p0.y + 2*(1-t)*t*p1.y + (t*t)*p2.y;
+        double x = ((1 - t) * (1 - t)) * p0.x + 2 * (1 - t) * t * p1.x + (t * t) * p2.x;
+        double y = ((1 - t) * (1 - t)) * p0.y + 2 * (1 - t) * t * p1.y + (t * t) * p2.y;
 
         return new Point(x, y);
     }
 
-    public double arc_length_param(double d){
-        return 0.0;
+    public ArrayList<Double> arc_length_param(double dist) {
+        ArrayList<Double> even_t = new ArrayList<>();
+        double last_t = 0.0;
+        even_t.add(0.0);
+        while (last_t < 1) {
+            double dDdt = this.length(this.derivative(last_t));
+            last_t += dist/dDdt;
+            even_t.add(last_t);
+        }
+        return even_t;
     }
 }
