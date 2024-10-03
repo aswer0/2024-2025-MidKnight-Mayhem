3373165a7e8b78f0ffce5a9a0c9974d6c6065e10
daniel.yang <daniel.yang@students.tka.org>
2024-10-02 17:19:23 -0700

    fixed path forward

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Experiments/Drivetrain/GVF/Path.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Experiments/Drivetrain/GVF/Path.java
@@ -6,10 +6,11 @@ import java.util.ArrayList;
 public class Path {
     ArrayList<BezierCurve> F;
     double T;
+    double close_T;
 
     public Path(ArrayList<ArrayList<Point>> cp){
         this.T = 0.0;
-
+        this.close_T = 0.0;
         for (int i=0; i<cp.size(); i+=3){
             BezierCurve bz = new BezierCurve(cp.get(i).get(0), cp.get(i).get(1), cp.get(i).get(2));
             F.add(bz);
@@ -34,18 +35,12 @@ public class Path {
 
     public Point forward(double t){
         int i = (int)Math.floor(t);
-        Point p0 = F.get(i).p0;
-        Point p1 = F.get(i).p1;
-        Point p2 = F.get(i).p2;
 
         //set new parametric parameter
         this.T = t;
 
         //calculate x and y of quadratic bezier curve as a parametric
-        double x = ((1-t-i)*(1-t-i))*p0.x + 2*(1-t-i)*(t-i)*p1.x + ((t-i)*(t-i))*p2.x;
-        double y = ((1-t-i)*(1-t-i))*p0.y + 2*(1-t-i)*(t-i)*p1.y + ((t-i)*(t-i))*p2.y;
-
-        return new Point(x, y);
+        return F.get(i).forward(t-i);
     }
 
     public double arc_length_param(double d){
