f11ba27fef864e8408244f6bf36e231b2030ae35
daniel.yang <daniel.yang@students.tka.org>
2024-10-04 15:27:02 -0700

    Changed F from ArrayList to Array

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Experiments/Drivetrain/GVF/Path.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Experiments/Drivetrain/GVF/Path.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Path {
-    ArrayList<BezierCurve> F;
+    BezierCurve[] F;
     double closest_T;
     int n_bz;
 
@@ -12,34 +12,35 @@ public class Path {
     public Path(Point[][] cp){
         this.closest_T = 0.0;
         this.n_bz = cp.length;
-
+        ArrayList<BezierCurve> temp_F = new ArrayList<>();
+        
         //add piecewise bezier curve
         for (int i=0; i<this.n_bz; i++){
-            BezierCurve bz = new BezierCurve(cp[i]);
-            F.add(bz);
+            temp_F.add(new BezierCurve(cp[i]));
         }
-    }
-
+        this.F = (BezierCurve[]) temp_F.toArray();
+    }             
+  
     //calculates Bezier curve
     public Point forward(double t){
         int i = (int)Math.floor(t);
-        return F.get(i).forward(t-i);
+        return F[i].forward(t-i);
     }
 
     public Point derivative(double t){
         int i = (int)Math.floor(t);
-        return F.get(i).derivative(t-i);
+        return F[i].derivative(t-i);
     }
 
     public void update_closest(Point p) {
         // Gets the closest point on the current curve to p
         int i = (int)Math.floor(closest_T);
-        F.get(i).update_closest(p);
-        closest_T = i+F.get(i).closest_T;
+        F[i].update_closest(p);
+        closest_T = i+F[i].closest_T;
 
         // Updates the new curve closest_T might be on
         i = (int)Math.floor(closest_T);
-        F.get(i).closest_T = closest_T-i;
+        F[i].closest_T = closest_T-i;
     }
 
     public Point get_v(Point p, double speed) {
@@ -48,11 +49,11 @@ public class Path {
 
         // Returns the final vector
         int i = (int)Math.floor(closest_T);
-        return F.get(i).get_v(p, speed);
+        return F[i].get_v(p, speed);
     }
 
     public ArrayList<Double> arc_length_param(double d){
         int i = (int)Math.floor(d);
-        return F.get(i).arc_length_param(d);
+        return F[i].arc_length_param(d);
     }
 }
