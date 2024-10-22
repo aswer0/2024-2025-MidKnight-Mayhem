73816c07c57ced02d38cd46aa1ef228420177a1f
daniel.yang <daniel.yang@students.tka.org>
2024-10-22 15:59:03 -0700

    new GVFPID

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Experiments/Opmodes/GVFPIDTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Experiments/Opmodes/GVFPIDTest.java
@@ -5,22 +5,30 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.Experiments.Drivetrain.GVF.Path;
-import org.firstinspires.ftc.teamcode.Experiments.Drivetrain.GVF.VectorField;
+import org.firstinspires.ftc.teamcode.Experiments.Drivetrain.GVF.GVFPID;
 import org.firstinspires.ftc.teamcode.Experiments.Drivetrain.Odometry;
 import org.firstinspires.ftc.teamcode.Experiments.Drivetrain.WheelControl;
 import org.opencv.core.Point;
 
 @Config
 @TeleOp
-public class GVFTest extends OpMode {
+public class GVFPIDTest extends OpMode {
     Odometry odometry;
     Path path;
-    VectorField vf;
+    GVFPID vf;
     WheelControl wheelControl;
 
     @Override
     public void init() {
         // Bezier control points
+        /*Point[][] cp = {
+                {
+                        new Point(5, 5),
+                        new Point(70, 40),
+                        new Point(116, 7)
+                },
+        };*/
+
         Point[][] cp = {
                 {
                         new Point(0, 72),
@@ -34,7 +42,7 @@ public class GVFTest extends OpMode {
         wheelControl = new WheelControl(hardwareMap, odometry);
 
         path = new Path(cp);
-        vf = new VectorField(wheelControl, odometry, path, 0.7,0.5, 20, 20, 0.1);
+        vf = new GVFPID(wheelControl, odometry, path, 0.7,0.5, -45);
     }
 
     @Override
@@ -44,9 +52,8 @@ public class GVFTest extends OpMode {
         telemetry.addData("xPos", odometry.opt.get_x());
         telemetry.addData("yPos", odometry.opt.get_y());
         telemetry.addData("heading", Math.toDegrees(odometry.opt.get_heading()));
-        telemetry.addData("vf_xPos", vf.odometry.opt.get_x());
-        telemetry.addData("turn_speed", vf.turn_speed);
-        telemetry.addData("speed", vf.speed);
+        //telemetry.addData("vf_xPos", vf.odometry.opt.get_x());
+        //telemetry.addData("speed", vf.speed);
         telemetry.addData("D", vf.D);
     }
 }
