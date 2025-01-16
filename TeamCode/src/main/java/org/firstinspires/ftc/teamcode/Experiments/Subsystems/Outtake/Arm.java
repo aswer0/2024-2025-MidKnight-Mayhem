2e490e22e3ef7832ba82482b24b2655cc0fcb87b
AnoyingShulker <ryan20090101@gmail.co>
2025-01-15 16:47:12 -0800

    change presets

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Experiments/Subsystems/Outtake/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Experiments/Subsystems/Outtake/Arm.java
@@ -12,13 +12,13 @@ public class Arm {
     Servo pivot_right;
     Servo tClaw;
 
-    public static Preset outtakeSpecimen1 = new Preset(0.9,0.4,0.35);
-    public static Preset outtakeSpecimen2 = new Preset(0.9,0.4,0.27);
+    public static Preset outtakeSpecimen1 = new Preset(0.9,0.4,0.3);
+    public static Preset outtakeSpecimen2 = new Preset(0.9,0.4,0.22);
     public static Preset outtakeSample = new Preset(0,0,0.5);
-    public static Preset intakeSpecimen = new Preset(0.1,0.4,0.8);
+    public static Preset intakeSpecimen = new Preset(0.1,0.4,0.75);
     public static Preset intakeSample = new Preset(0,0,0.5);
-    public static Preset idlePosition = new Preset(0.1,0.7,0.69);
-    public static Preset autoStartPosition = new Preset(0.9,0.7,0.75);
+    public static Preset idlePosition = new Preset(0.1,0.7,0.64);
+    public static Preset autoStartPosition = new Preset(0.9,0.7,0.7);
 
     public static double clawClosePos = 0.485;
     public static double clawOpenPos = 0.7;
@@ -27,7 +27,7 @@ public class Arm {
         this.outtake_arm_far = hardwareMap.get(Servo.class, "oAF");
         this.outtake_arm_near  = hardwareMap.get(Servo.class, "oAN");
         this.pivot_left = hardwareMap.get(Servo.class, "oPL");
-        this.pivot_right = hardwareMap.get(Servo.class, "oPR ");
+        this.pivot_right = hardwareMap.get(Servo.class, "oPR");
         this.tClaw = hardwareMap.get(Servo.class, "tClaw");
     }
     public void applyPreset(Preset preset) {
