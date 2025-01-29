b5f88b87806913cf012a5f266a716e8fe0d9dbb9
daniel.yang <daniel.yang@students.tka.org>
2025-01-29 15:28:37 -0800

    substitute pid for more accuracy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalPrograms/SpecimenAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalPrograms/SpecimenAuto.java
@@ -1,5 +1,5 @@
 package org.firstinspires.ftc.teamcode.FinalPrograms;
-
+//
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
