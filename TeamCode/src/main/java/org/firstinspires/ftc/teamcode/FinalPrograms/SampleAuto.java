9b0d78c7745a1c3bedcdcd8fa1efe05f8a754759
daniel.yang <daniel.yang@students.tka.org>
2025-02-06 10:11:09 -0800

    finished sample auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalPrograms/SampleAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalPrograms/SampleAuto.java
@@ -10,22 +10,26 @@ import org.firstinspires.ftc.teamcode.Experiments.Drivetrain.GVF.VectorField;
 import org.firstinspires.ftc.teamcode.Experiments.Drivetrain.GVF.BCPath;
 import org.firstinspires.ftc.teamcode.Experiments.Drivetrain.Odometry;
 import org.firstinspires.ftc.teamcode.Experiments.Drivetrain.WheelControl;
+import org.firstinspires.ftc.teamcode.Experiments.Subsystems.Intake.HorizontalSlides;
 import org.firstinspires.ftc.teamcode.Experiments.Subsystems.Intake.Intake;
 import org.firstinspires.ftc.teamcode.Experiments.Subsystems.Outtake.Lift;
 import org.firstinspires.ftc.teamcode.Experiments.Subsystems.Outtake.Arm;
-import org.firstinspires.ftc.teamcode.Experiments.Subsystems.Outtake.Manipulator;
+//import org.firstinspires.ftc.teamcode.Experiments.Subsystems.Outtake.Manipulator;
+import org.firstinspires.ftc.teamcode.Experiments.Utils.Alliance;
 import org.firstinspires.ftc.teamcode.Experiments.Utils.Sensors;
 import org.opencv.core.Point;
 
 @Autonomous
 @Config
 public class SampleAuto extends OpMode {
+    double intake_slide_extend = -450;
     double corner_offset = 18;
     Point start_point;
-    //Point get_specimen_target;
+    double get_sample_x, get_sample_y;
     Point deposit_sample_target;
-    Point hang_target;
+    Point park_target;
 
+    int deposit_state;
     ElapsedTime timer;
     Sensors sensors;
 
@@ -36,16 +40,20 @@ public class SampleAuto extends OpMode {
 
     State state = State.pid;
     Lift lift;
-    Manipulator manipulator;
+    Arm arm;
+    HorizontalSlides intakeSlides;
 
     Intake intake;
-    //Arm arm;
+
+    Alliance alliance = Alliance.red;
 
     enum State {
         pid,
         deposit_sample,
+        move_to_intake_sample,
         intake_sample,
-        hang
+        transfer_sample,
+        park
     }
 
     @Override
@@ -56,8 +64,12 @@ public class SampleAuto extends OpMode {
     @Override
     public void init() {
         start_point = new Point(7.875, 96);
+        get_sample_x = 25;
+        get_sample_y = 120;
+        deposit_state = 0;
         deposit_sample_target = new Point(corner_offset, 144-corner_offset);
-        hang_target = new Point(72, 96);
+        park_target = new Point(20, 40);
+        //ascent_target = new Point(72, 96);
 
         Point[][] follow_path = {{
                 new Point(7.875, 21.3),
@@ -81,48 +93,111 @@ public class SampleAuto extends OpMode {
         wheelControl.change_mode(DcMotor.ZeroPowerBehavior.BRAKE);
 
         lift = new Lift(hardwareMap);
-        manipulator = new Manipulator(hardwareMap);
-
+        arm = new Arm(hardwareMap);
         intake = new Intake(hardwareMap, sensors);
-        //arm = new Arm(hardwareMap);
+        intakeSlides = new HorizontalSlides(hardwareMap);
 
-        manipulator.closeClaw();
+        arm.toAutoStartPosition();
+        arm.closeClaw();
+    }
+
+    @Override
+    public void init_loop() {
+        if (gamepad1.left_bumper) {
+            alliance = Alliance.blue;
+        } else if (gamepad1.right_bumper) {
+            alliance = Alliance.red;
+        }
+
+        telemetry.addData("Alliance", alliance);
     }
 
     @Override
     public void loop() {
         odometry.opt.update();
+        lift.update();
+        telemetry.update();
+
         switch(state) {
             case pid:
                 intake.up();
                 intake.stop();
-                manipulator.closeClaw();
-                //lift.setPosition(1600);
+                arm.closeClaw();
+                arm.outtakeSample();
+                lift.toHighBasket();
 
-                vf.move_to_point(deposit_sample_target, 135, 0.5);
+                vf.move_to_point(deposit_sample_target, 135, 0.7);
 
                 if (vf.dist_to_end() < 1 || timer.milliseconds() > 5000) {
                     timer.reset();
                     state = State.deposit_sample;
                 }
 
+                break;
+
             case deposit_sample:
-                if (timer.milliseconds() > 100) {
-                    manipulator.openClaw();
-                }
+                intake.up();
+                intake.stop();
+                arm.openClaw();
+                arm.outtakeSample();
 
-                if (timer.milliseconds() > 1000) {
+                if (timer.milliseconds() > 300) {
                     timer.reset();
-                    state = State.hang;
+                    deposit_state++;
+                    if (deposit_state <= 3) {
+                        state = State.move_to_intake_sample;
+                    } else {
+                        state = State.park;
+                    }
                 }
 
-            case hang:
-                manipulator.closeClaw();
-                lift.setPosition(1200);
-                vf.move_to_point(hang_target, -90, 0.5);
+                break;
+
+            case move_to_intake_sample:
+                intake.up();
+                intake.stop();
+
+                vf.move_to_point(new Point(get_sample_x, get_sample_y), 0, 0.7);
+
+                if (vf.dist_to_end() < 1 || timer.milliseconds() > 5000) {
+                    timer.reset();
+                    state = State.intake_sample;
+                }
+
+                break;
+
+            case intake_sample:
+                arm.intakeSample();
+                arm.openClaw();
+                intake.down();
+                intake.intake();
+                intakeSlides.setPosition(intake_slide_extend);
+
+                if (intake.hasCorrectSample(true) || timer.milliseconds() >= 2000) {
+                    timer.reset();
+                    get_sample_y += 10;
+                    state = State.transfer_sample;
+                }
+
+                break;
+
+            case transfer_sample:
+                intakeSlides.setPosition(0);
+                intake.up();
+                arm.intakeSample();
+
+                if (timer.milliseconds() >= 1000) {
+                    arm.closeClaw();
+                }
+                if (timer.milliseconds() >= 1300) {
+                    timer.reset();
+                    state = State.pid;
+                }
+
+                break;
+
+            case park:
+                vf.move_to_point(park_target, 0, 0.7);
         }
-        lift.update();
-        intake.up();
-        telemetry.update();
     }
 }
